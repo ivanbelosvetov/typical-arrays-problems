ad97fbaaed3e6651423f047c10fa7b056750b781
ivanbelosvetov <79517671+ivanbelosvetov@users.noreply.github.com>
2021-03-14 23:56:31 +0200

    init: start typical arrays problems task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,35 @@
-
 exports.min = function min (array) {
-  return 0;
+if(!array || array.length == 0) {
+return 0;
+}
+
+array.sort(function(x,y) {
+      return x-y;
+    });
+
+return array[0];
+  //return 0;
 }
 
 exports.max = function max (array) {
-  return 0;
+
+if(!array || array.length ==0) {
+return 0;
+}
+
+array.sort((y,x) => y-x);
+
+return array[array.length - 1];
+  //return 0;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+if(!array || array.length == 0) {
+return 0;
+}
+
+const reducer = (accumulator, currentValue) => accumulator + currentValue;
+
+return array.reduce(reducer) / array.length;
+  //return 0;
 }
